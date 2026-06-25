1e6e254e9e690ee506ba88c0a371749ad3add2a6
GitEimaths <jimmiepotter@gmail.com>
2026-06-26 05:20:55 +0700

    Deploy app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/eimaths-bounty-hunter/' : '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
   },
-});
+}));
